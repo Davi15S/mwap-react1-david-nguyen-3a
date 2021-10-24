69db0b31d173ab8f77ae263a9642958e6df21863
Davi15S <nguyenxd072@gmail.com>
2021-10-24 18:28:05 +0200

    update

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function Footer() {
-    return (
-        <footer className="App-footer">
-        <p className="footer">Copyright © David Nguyen</p>
-      </footer>
-    );
+  return (
+    <footer className="App-footer">
+      <p className="footer">Copyright © David Nguyen</p>
+    </footer>
+  );
 }
 
 export default Footer;
